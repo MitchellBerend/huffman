88f3c3659c9f19c657568dbbb322fa5aea27fc71
Mitchell Berendhuysen <45570310+MitchellBerend@users.noreply.github.com>
2025-04-26 18:03:34 +0200

    Added cli interface to encode and decode files (#5)
    This pr adds a way to actually encode and decode separately

--- decode_test.go
+++ decode_test.go
@@ -16,3 +16,14 @@ func TestDecode(t *testing.T) {
 		string(decoded),
 	)
 }
+
+func TestImage(t *testing.T) {
+	blob := []byte{0x3, 0x54, 0xfe, 0x0}
+
+	decoded := Decode(blob, ac21)
+
+	assert.Equal(t,
+		content,
+		string(decoded),
+	)
+}
